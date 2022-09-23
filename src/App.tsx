8f62fa35d3f4e8a52f644cc18c091be218394ae5
Rodney Espinoza <respinoza@thirtyonegifts.com>
2022-09-23 15:20:03 -0600

    Tarea1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const mystyle = {
+    textAlign: "left",
+  };
+  const listStyle = {
+    listStyle:'none'
+  }
+
+  const [toDo, seeData] = useState<any[]>([]);
+  const url = "https://jsonplaceholder.typicode.com/todos";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          return Error("Oh no");
+        }
+        return res.json();
+      })
+      .then((data) => seeData(data));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Rodney Espinoza Tarea1</p>
+        <p>Render, estilos inline, checkbox</p>
+        <ul style ={{listStyle:'none'}}>
+          {toDo.map((list) => (
+            <li>
+              <div key={list.id} style ={{textAlign: "left"}}>  
+                {list.id} - {list.title}
+                <input
+                  type="checkbox"
+                  id={list.id}
+                  value={list.completed}
+                  checked={list.completed}
+                />
+              </div><br />
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
